38f562bf53114fa8e917455d8584900748ae8e8c
Anthony Luque <kasuru@hotmail.com>
2021-02-02 15:11:03 -0500

    [ADD] Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# prueba_tecnica_react
+# Prueba técnica react
+
+- Consumir datos de la API de https://openweathermap.org/
+- Mostrar las ciudades en cards
+- Buscador (Sin implementar)
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './search.css'
 
 class Search extends React.Component {
+    state = {
+        search: ''
+    }
+
+    onChange = async e=> {
+        e.persist();
+        await this.setState({search: e.target.value});
+        console.log(this.state.search)
+    }
+
     render(){
         return (
             <section id='search_container'>
-                <input type='text' placeholder='Search other cities...'></input>
+                <input 
+                    type='text' 
+                    placeholder='Search other cities...'
+                    value={this.state.search}
+                    onChange={this.onChange}
+                ></input>
             </section>
         )
     }
