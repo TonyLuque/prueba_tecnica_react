ab9dea7e34bbeeb2fa18e04f50adea0bb963a8ac
Anthony Luque <kasuru@hotmail.com>
2021-02-02 13:35:48 -0500

    [FIX] card tag

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,7 +7,7 @@ class Card extends React.Component {
         <section className='card'>
             <p id='city_card' className='text_card'>{this.props.city}, {this.props.country}</p>
             <article>
-                <img src={'http://openweathermap.org/img/wn/'+this.props.icon+'@2x.png'}/>
+                <img alt='icon' src={'http://openweathermap.org/img/wn/'+this.props.icon+'@2x.png'}/>
                 <div>
                     <p id='time' className='text_card'>{this.props.time}</p>
                     <p id='temp' className='text_card'>{this.props.temp}ºC</p>
